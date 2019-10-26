00ca37d99bcd70a987bb66fdb34b2223831e0f0c
Nick Fiacco <nicholas.r.fiacco.17@dartmouth.edu>
2019-10-26 11:46:32 -0700

    Add basic input functionality and data passing
    Create a text box to input the Open API JSON specification,
    a button that triggers the data to be passed to another component,
    and a component that will be used to render the operations.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      apiData: null
+    };
+
+    this.generateApiExplorer = this.generateApiExplorer.bind(this);
+  }
+
+  generateApiExplorer(newApiData) {
+    console.log("did the thing");
+    this.setState({
+      apiData: newApiData
+    });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <h1 className="title"> Open API Explorer </h1>
+        <h2 className="title"> Enter an Open API JSON specification into the text box to get started </h2>
+        <SpecInput generateApiExplorer={this.generateApiExplorer} />
+        <Explorer apiData={this.state.apiData} />
+      </div>
+    );
+  }
 }
 
-export default App;
+class SpecInput extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            generateApiExplorer: props.generateApiExplorer,
+            apiData: null
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            apiData: event.target.value
+        });
+    }
+
+    handleClick(event) {
+        this.state.generateApiExplorer(this.state.apiData);
+    }
+
+    render() {
+        return (
+            <div className="wrapper">
+                <textarea className="spec-input" onChange={this.handleChange} />
+                <input className="spec-submit" type="button" value="Create API Client!" onClick={this.handleClick} />
+            </div>
+        );
+    }
+}
+
+class Explorer extends React.Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <h1>{this.props.apiData}</h1>
+      </div>
+    );
+  }
+}
